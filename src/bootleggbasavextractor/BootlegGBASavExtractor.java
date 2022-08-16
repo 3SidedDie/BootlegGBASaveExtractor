5c79c0d549cddef69a7b025c49da266a3f53a735
3SidedDie <111396955+3SidedDie@users.noreply.github.com>
2022-08-16 20:01:44 -0300

    Ver 0.6
    Added files for version 0.6

---
+++ src/bootleggbasavextractor/BootlegGBASavExtractor.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bootleggbasavextractor;
+
+import java.awt.Component;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author SummonerArthur
+ */
+public class BootlegGBASavExtractor {
+    
+    
+    
+    public static Screen scr;
+    
+    public static Thread thr0;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JOptionPane.showConfirmDialog((Component) null, "Program by: 3SidedDie\n\nSpecial thanks to:\n''watermagician1996'' on Reddit\n''Im a blisy'' on Youtube.","CREDITS", JOptionPane.DEFAULT_OPTION);
+        
+        scr = new Screen();
+        thr0 = new Thread(scr);
+        thr0.start();
+        
+    }
+    
+}
